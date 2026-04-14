62a293527d5a281e00ad28c4e8dc3c90dd199c6c
unknown <thilakeswarandg2007@gmail.com>
2026-04-14 20:10:03 +0530

    final cors fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,12 @@ app = FastAPI(
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost",
+        "http://localhost:56707",
+        "http://localhost:50865",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
